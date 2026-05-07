c52414ca59acdd238eef3a5549ade9ff034bfa32
Yasin Derya Bilgin <ydbilgin@gmail.com>
2026-05-08 00:50:48 +0300

    feat: ClearTilemaps editor tool -- wipe all tilemap tile data for fresh F1 placement

---
+++ Assets/Editor/DevTools/ClearTilemaps.cs
@@ -0,0 +1,48 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+namespace RIMA.Editor.DevTools
+{
+    public static class ClearTilemaps
+    {
+        [MenuItem("RIMA/Clear All Tilemap Tiles")]
+        public static void ClearAllTilemapTiles()
+        {
+            Tilemap[] tilemaps = Object.FindObjectsOfType<Tilemap>(true);
+            int clearedCount = 0;
+            int totalRemoved = 0;
+
+            foreach (Tilemap tilemap in tilemaps)
+            {
+                int tileCount = CountTiles(tilemap);
+                Debug.Log($"ClearTilemaps: {tilemap.name} had {tileCount} tiles.");
+
+                tilemap.ClearAllTiles();
+                clearedCount++;
+                totalRemoved += tileCount;
+            }
+
+            EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+            Debug.Log($"ClearTilemaps: cleared {clearedCount} tilemaps, removed {totalRemoved} tiles.");
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        private static int CountTiles(Tilemap tilemap)
+        {
+            int count = 0;
+
+            foreach (Vector3Int position in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (tilemap.HasTile(position))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
